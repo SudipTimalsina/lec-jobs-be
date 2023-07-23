84f43608ad192bec7517c116a14f438cdf65e623
SudipTimalsina <timalsinasudip11@gmail.com>
2023-07-23 19:38:36 +0545

    Updates

--- src/app.js
+++ src/app.js
@@ -49,8 +49,52 @@ User.create({
   is_active: true,
   followers: ["username123", "user234", "user543"],
   followings: ["username123", "user234", "user543", "user555"],
-}).then(()=>{
-    console.log("User Created");
+}).then(() => {
+  console.log("User Created");
+});
+
+// trying out to do the same for the post section
+const postSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  location: String,
+  job_type: String,
+  pay_rate_per_hr_dollar: Number,
+  skills: [{ type: String }],
+  liked_by: [{ type: String }],
+  viewed_by: [{ type: String }],
+  id: Number,
+  user_id: Number,
+  post_by_username: String,
+  post_by_fullname: String,
+  post_date: String,
+  comments: [{ type: String }],
+});
+const Post = mongoose.model("Post", postSchema);
+User.createCollection()
+  .then((col) => {
+    console.log("Collection", col, "Created");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+Post.create({
+  title: "Python Developer Required",
+  description: "For a client project Python Developer is required",
+  location: "Lalitpur",
+  job_type: "Full Time",
+  pay_rate_per_hr_dollar: 25.0,
+  skills: ["Python", "JS", "HTML"],
+  liked_by: ["test111", "test1", "test122"],
+  viewed_by: ["test111", "test1", "test123"],
+  id: 2,
+  user_id: 1,
+  post_by_username: "Sudip",
+  post_by_fullname: "Sudip Timalsina",
+  post_date: "2023-06-10T09:24:07.659034",
+  comments: [],
+}).then(() => {
+  console.log("Poster Detail is Created");
 });
 
 app.get("/", (req, res) => {
