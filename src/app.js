c24698442cda991eb211247ece2357c3555b28c9
SudipTimalsina <timalsinasudip11@gmail.com>
2023-07-29 09:26:56 +0545

    User data sent from backend

--- src/app.js
+++ src/app.js
@@ -37,21 +37,7 @@ User.createCollection()
   .catch((err) => {
     console.log(err);
   });
-User.create({
-  email: "test@test.com",
-  username: "sudip",
-  fullname: "Sudip Timalsina",
-  title: "Software Developer",
-  skills: ["JS", "PHP", "JAVA"],
-  address: "Kathmandu, Nepal",
-  job_type: "Full Time",
-  id: 1,
-  is_active: true,
-  followers: ["username123", "user234", "user543"],
-  followings: ["username123", "user234", "user543", "user555"],
-}).then(() => {
-  console.log("User Created");
-});
+
 
 // trying out to do the same for the post section
 const postSchema = new mongoose.Schema({
@@ -78,38 +64,68 @@ User.createCollection()
   .catch((err) => {
     console.log(err);
   });
-Post.create({
-  title: "Python Developer Required",
-  description: "For a client project Python Developer is required",
-  location: "Lalitpur",
-  job_type: "Full Time",
-  pay_rate_per_hr_dollar: 25.0,
-  skills: ["Python", "JS", "HTML"],
-  liked_by: ["test111", "test1", "test122"],
-  viewed_by: ["test111", "test1", "test123"],
-  id: 2,
-  user_id: 1,
-  post_by_username: "Sudip",
-  post_by_fullname: "Sudip Timalsina",
-  post_date: "2023-06-10T09:24:07.659034",
-  comments: [],
-}).then(() => {
-  console.log("Poster Detail is Created");
-});
+// Post.create({
+//   title: "Python Developer Required",
+//   description: "For a client project Python Developer is required",
+//   location: "Lalitpur",
+//   job_type: "Full Time",
+//   pay_rate_per_hr_dollar: 25.0,
+//   skills: ["Python", "JS", "HTML"],
+//   liked_by: ["test111", "test1", "test122"],
+//   viewed_by: ["test111", "test1", "test123"],
+//   id: 2,
+//   user_id: 1,
+//   post_by_username: "Sudip",
+//   post_by_fullname: "Sudip Timalsina",
+//   post_date: "2023-06-10T09:24:07.659034",
+//   comments: [],
+// }).then(() => {
+//   console.log("Poster Detail is Created");
+// });
+
+
+
+
+
+
 
 app.get("/", (req, res) => {
   res.status(200).send("This is response from BE");
 });
 
 app.get("/api/v1/posts", (req, res) => {
+  const user = User.find({id: 1})
   const posts = fs.readFileSync("./data/posts.json", "utf-8").toString();
   res.status(200).send(posts);
 });
 
-app.get("/api/v1/user", (req, res) => {
-  const user = fs.readFileSync("./data/user.json", "utf-8").toString();
-  res.status(200).send(user);
+app.get("/api/v1/user", async (req, res) => {
+  const user = await User.find({id: 1})
+  // const user = fs.readFileSync("./data/user.json", "utf-8").toString();
+  res.status(200).send(user[0]);
 });
+
+
+app.post("/api/v1/user", (req , resp)=>{
+  const id = req.query.id;
+  const newUser = {
+  email: "test@test.com",
+  username: "sudip",
+  fullname: "Sudip Timalsina",
+  title: "Software Developer",
+  skills: ["JS", "PHP", "JAVA"],
+  address: "Kathmandu, Nepal",
+  job_type: "Full Time",
+  id: id,
+  is_active: true,
+  followers: [],
+  followings: [],
+}
+  User.create(newUser).then((createdUser) => {
+    console.log("User Created");
+    resp.status(200).send(createdUser);
+  });
+})
 
 app.listen(PORT, () => {
   console.log("App is Running on port " + PORT);
